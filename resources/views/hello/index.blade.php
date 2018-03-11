b80ede4bdc241174a817560f8bc6b2055a3a34cc
Yasufumi Sakakura <yasufumisakakura@gmail.com>
2018-03-11 22:19:51 +0900

    asigned message template to hello page

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,8 +8,19 @@
 @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
+
+    @component('components.message')
+        @slot('msg_title')
+        CAUTION!
+        @endslot
+
+        @slot('msg_content')
+        これはメッセージの表示です。
+        @endslot
+    @endcomponent
+
 @endsection
 
 @section('footer')
-    copyright 2017 tuyano.
+    copyright 2017 sakakura.
 @endsection
