9da414c02f0597021a0d84a40c095b3af16bd6e4
Yasufumi Sakakura <yasufumisakakura@gmail.com>
2018-03-14 22:50:04 +0900

    created the form with the validation

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -6,10 +6,37 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>これは<middleware>google.com</middleware>へリンクです</p>
-    <p>これは<middleware>yahoo.co.jp</middleware>へのリンクです</p>
-
+    <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+    <div>
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <table>
+        <form class="/hello" method="post">
+            {{ csrf_field() }}
+            <tr>
+                <th>name: </th>
+                <td><input type="text" name="name" value="{{old('name')}}"></td>
+            </tr>
+            <tr>
+                <th>mail: </th>
+                <td><input type="text" name="mail" value="{{old('mail')}}"></td>
+            </tr>
+            <tr>
+                <th>age: </th>
+                <td><input type="text" name="age" value="{{old('age')}}"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="send"></td>
+            </tr>
+        </form>
+    </table>
 @endsection
 
 @section('footer')
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Route::get('/', function () {
     return view ('welcome');
 });
 
-Route::get('hello', 'HelloController@index')->middleware('helo');
+Route::get('hello', 'HelloController@index');
+Route::post('hello', 'HelloController@post');
 
 Route::get('/tasks', function () {
 
