788e071425a8e512dde4f841d02bb03deef57b27
Yasufumi Sakakura <yasufumisakakura@gmail.com>
2018-03-16 23:47:32 +0900

    used DB::create

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,13 +10,29 @@ class HelloController extends Controller
 {
     public function index (Request $request)
     {
-        if (isset($request->id))
-        {
-            $param = ['id' => $request->id];
-            $items = DB::select('select * from people where id = :id', $param);
-        } else {
-            $items = DB::select('select * from people');
-        }
+        $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
+    }
+
+    public function post(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::get('/', function () {
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
 
 Route::get('/tasks', function () {
 
