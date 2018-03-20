b1d6b46c041a18ff6043da3b50c91f0dc63af856
Yasufumi Sakakura <yasufumisakakura@gmail.com>
2018-03-20 23:18:48 +0900

    used Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@delete');
 Route::post('hello/del', 'HelloController@remove');
+Route::get('person', 'PersonController@index');
 
 Route::get('/tasks', function () {
 
