67613f6566b1d97b667de672a474b37a774b23c8
Yasufumi Sakakura <yasufumisakakura@gmail.com>
2018-03-20 23:22:19 +0900

    expanded the Person class

--- app/Person.php
+++ app/Person.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     //
+    public function getDate()
+    {
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+    }
 }
